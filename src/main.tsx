dd06f3632f182e32ead453d8b3c0398dc222f1f3
Harang Ju <harangju@gmail.com>
2025-11-12 12:24:41 -0500

    fix og-image

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,6 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-import ogImage from "./assets/og-image.png";
-
-const updateOgImage = () => {
-  const ogImageUrl = new URL(ogImage, window.location.origin).href;
-  const ogImageMeta = document.querySelector('meta[property="og:image"]');
-  if (ogImageMeta) {
-    ogImageMeta.setAttribute("content", ogImageUrl);
-  }
-};
-
-updateOgImage();
 
 const rootElement = document.getElementById("root");
 
