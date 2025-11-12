507dbc504a57a98041169d880361444ee86bd7d7
Harang Ju <harangju@gmail.com>
2025-11-12 12:10:42 -0500

    fix og-image not in build

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import ogImage from "./assets/og-image.png";
+
+const updateOgImage = () => {
+  const ogImageUrl = new URL(ogImage, window.location.origin).href;
+  const ogImageMeta = document.querySelector('meta[property="og:image"]');
+  if (ogImageMeta) {
+    ogImageMeta.setAttribute("content", ogImageUrl);
+  }
+};
+
+updateOgImage();
 
 const rootElement = document.getElementById("root");
 
